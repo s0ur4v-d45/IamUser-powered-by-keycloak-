1fb6d98465ff400afe73f799c917489c1332acb4
Sourav Das <souravdas78745@gmail.com>
2024-01-10 22:03:00 +0530

    checkstyle issues completely resolved

--- src/main/java/com/iam_user_management/keycloak/service/RoleServiceImpl.java
+++ src/main/java/com/iam_user_management/keycloak/service/RoleServiceImpl.java
@@ -23,86 +23,94 @@ public class RoleServiceImpl implements RoleService {
     private final KeycloakProperties keycloakProperties;
     private final RoleRepository roleRepository;
 
-    private final String CLIENT_ID = "12f86e26-e4cc-443f-bbe9-44547b58d3a5";
-
     @Override
-    public List<RoleDTO> findAllRealmRoles(String realm) {
+    public List<RoleDTO> findAllRealmRoles(final String realm) {
         return roleRepository.findAllByClientRoleFalseAndRealmRealm(realm).stream().map(roleMapper::mapEntityToDto)
                 .toList();
     }
 
     @Override
-    public RoleDTO findRoleById(String roleId) {
+    public RoleDTO findRoleById(final String roleId) {
         return roleMapper.mapEntityToDto(roleRepository.findById(roleId).orElseThrow(NotFoundException::new));
     }
 
     @Override
-    public RoleDTO createRealmRole(RoleDTO roleDTO) {
+    public RoleDTO createRealmRole(final RoleDTO roleDTO) {
         keycloakClient.realm(roleDTO.getRealmName()).roles().create(roleMapper.mapRoleDtoToRoleRepresentation(roleDTO));
         RoleDTO createdRealmRole = roleMapper.mapRoleRepresentationToRoleDto(
                 keycloakClient.realm(roleDTO.getRealmName()).roles().get(roleDTO.getRealmName()).toRepresentation());
         roleRepository.save(roleMapper.mapDtoToEntity(createdRealmRole));
-    return createdRealmRole;
+        return createdRealmRole;
     }
 
     @Override
-    public void updateRealmRole(String realmName, String roleName, RoleDTO updatedRole) {
+    public void updateRealmRole(final String realmName,final  String roleName,final RoleDTO updatedRole) {
         RoleDTO existingRole = roleMapper.mapEntityToDto(
                 roleRepository.findByNameAndRealmRealmAndClientRoleFalse(roleName,realmName)
                         .orElseThrow(NotFoundException::new));
-        existingRole.setDescription(updatedRole.getDescription()!=null ?updatedRole.getDescription():existingRole.getDescription());
-        existingRole.setAttributes(updatedRole.getAttributes() !=null ? updatedRole.getAttributes() :existingRole.getAttributes());
+        existingRole.setDescription(updatedRole.getDescription() != null
+                 ? updatedRole.getDescription()
+                 : existingRole.getDescription());
+        existingRole.setAttributes(updatedRole.getAttributes() != null
+                ? updatedRole.getAttributes()
+                : existingRole.getAttributes());
         roleRepository.save(roleMapper.mapDtoToEntity(existingRole));
-        keycloakClient.realm(realmName).roles().get(roleName).update(roleMapper.mapRoleDtoToRoleRepresentation(existingRole));
+        keycloakClient.realm(realmName).roles().get(roleName)
+                .update(roleMapper.mapRoleDtoToRoleRepresentation(existingRole));
     }
 
     @Override
-    public void deleteRealmRole(String realmName, String roleName) {
+    public void deleteRealmRole(final String realmName, final String roleName) {
         roleRepository.findByNameAndRealmRealm(roleName , realmName).orElseThrow(NotFoundException::new);
         keycloakClient.realm(realmName).roles().get(roleName).remove();
         roleRepository.deleteByName(roleName);
     }
 
-
     @Override
-    public RoleDTO createClientRole(String realmName, String roleName) {
+    public RoleDTO createClientRole(final String realmName,final String roleName) {
 
-            RealmResource realmResource = keycloakClient.realm(realmName);
-            ClientResource clientResource = realmResource.clients().get(CLIENT_ID);
+        RealmResource realmResource = keycloakClient.realm(realmName);
+        ClientResource clientResource = realmResource.clients().get(keycloakProperties.getContainerId());
 
-            RolesResource rolesResource = clientResource.roles();
+        RolesResource rolesResource = clientResource.roles();
 
-                List<RoleRepresentation> roleList = rolesResource.list();
-                if (roleList == null || roleList.isEmpty()) {
-                    RoleRepresentation roloRep = new RoleRepresentation(roleName, roleName, true);
-                    rolesResource.create(roloRep);
-                } else {
-                    boolean exists = false;
-                    for (RoleRepresentation r : roleList) {
-                        if (r.getName().equals(roleName)) {
-                            exists = true;
-                            break;
-                        }
-                    }
-                    if (!exists) {
-                        RoleRepresentation newRole = new RoleRepresentation(roleName, roleName, true);
-                        rolesResource.create(newRole);
-                    }
+        List<RoleRepresentation> roleList = rolesResource.list();
+        if (roleList == null || roleList.isEmpty()) {
+            RoleRepresentation roloRep = new RoleRepresentation(roleName, roleName, true);
+            rolesResource.create(roloRep);
+        } else {
+            boolean exists = false;
+            for (RoleRepresentation r : roleList) {
+                if (r.getName().equals(roleName)) {
+                    exists = true;
+                    break;
+                }
             }
-        RoleDTO createdRole = roleMapper.mapRoleRepresentationToRoleDto(keycloakClient.realm(realmName).clients().get(CLIENT_ID).roles().get(roleName).toRepresentation());
+            if (!exists) {
+                RoleRepresentation newRole = new RoleRepresentation(roleName, roleName, true);
+                rolesResource.create(newRole);
+            }
+        }
+        RoleDTO createdRole = roleMapper.mapRoleRepresentationToRoleDto(keycloakClient.realm(realmName)
+                .clients().get(keycloakProperties.getContainerId())
+                .roles()
+                .get(roleName).toRepresentation());
         roleRepository.save(roleMapper.mapDtoToEntity(createdRole));
         return createdRole;
     }
 
     @Override
-    public List<RoleDTO> findAllClientRoles(String realm) {
-        return roleRepository.findAllByClientRoleTrueAndRealmRealm(realm).stream().map(roleMapper::mapEntityToDto).toList();
+    public List<RoleDTO> findAllClientRoles(final String realm) {
+        return roleRepository.findAllByClientRoleTrueAndRealmRealm(realm)
+                .stream()
+                .map(roleMapper::mapEntityToDto).toList();
     }
 
     @Override
-    public void deleteClientRole(String realmName, String roleName) {
-        roleRepository.findByNameAndRealmRealmAndClientRoleTrue(roleName , realmName).orElseThrow(NotFoundException::new);
-        keycloakClient.realm(realmName).clients().get(CLIENT_ID)
+    public void deleteClientRole(final String realmName, final String roleName) {
+        roleRepository.findByNameAndRealmRealmAndClientRoleTrue(roleName , realmName)
+                .orElseThrow(NotFoundException::new);
+        keycloakClient.realm(realmName).clients().get(keycloakProperties.getContainerId())
                 .roles().deleteRole(roleName);
         roleRepository.deleteByName(roleName);
     }
